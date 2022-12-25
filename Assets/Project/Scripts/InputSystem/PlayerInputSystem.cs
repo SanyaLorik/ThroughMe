d84984264dbd98e1c17d25b55f9b9feda2050628
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2022-12-25 21:35:21 +0300

    add: Joystick

---
+++ Assets/Project/Scripts/InputSystem/PlayerInputSystem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace ThroughMe.InputSystem
+{
+    public class PlayerInputSystem : MonoBehaviour
+    {
+        [SerializeField] private Joystick _joystick;
+
+
+    }
+}
