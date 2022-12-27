4e358d5c8d722fbace92c2504babf83122ab6cc5
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2022-12-27 20:53:08 +0300

    update: Obstalce

--- Assets/Project/Scripts/Entities/Obstacle/Obstacle.cs
+++ Assets/Project/Scripts/Entities/Obstacle/Obstacle.cs
@@ -7,21 +7,21 @@ namespace ThroughMe.Entities
     [RequireComponent(typeof(Rigidbody))]
     public abstract class Obstacle : MonoBehaviour, IObstacle
     {
+        [SerializeField][Min(0)] private float _destoyAfterPortal;
         [SerializeField][Min(0)] private float _speed;
 
         private Rigidbody _rigidbody;
 
+        private bool _isInvoked = false;
+
         private void Awake() =>
             _rigidbody = GetComponent<Rigidbody>();
 
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.transform.GetComponent<IPortal>() is null)
-                return;
+        private void OnTriggerEnter(Collider other) =>
+            Collision(other.transform, _destoyAfterPortal);
 
-            PortalReached?.Invoke();
-            Destroy(gameObject);
-        }
+        private void OnCollisionEnter(Collision collision) =>
+            Collision(collision.transform);
 
         public Action PortalReached { private get; set; }
 
@@ -35,5 +35,18 @@ namespace ThroughMe.Entities
 
         public void LookAt(Vector3 target) =>
             transform.LookAt(target);
+
+        private void Collision(Transform source, float time = 0)
+        {
+            if (source.GetComponent<IPortal>() is null)
+                return;
+
+            if (_isInvoked == false)
+                PortalReached?.Invoke();
+
+            _isInvoked = true;
+
+            Destroy(gameObject, time);
+        }
     }
 }
