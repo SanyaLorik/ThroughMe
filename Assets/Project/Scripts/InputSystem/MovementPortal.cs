3e05a915e158ee0118cc9885eb1139b0343f7c68
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2022-12-27 19:49:22 +0300

    delete: new InpuSystem

--- Assets/Project/Scripts/InputSystem/MovementPortal.cs
+++ Assets/Project/Scripts/InputSystem/MovementPortal.cs
@@ -1,12 +1,21 @@
 ﻿using ThroughMe.Movement;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace ThroughMe.InputSystem
 {
     [RequireComponent(typeof(Image))]
-    public class MovementPortal : MonoBehaviour, IDirection
+    public class MovementPortal : MonoBehaviour, IDirection, IDragHandler
     {
         public Vector2 Direction => throw new System.NotImplementedException();
+
+        private Vector2 _first = Vector2.zero;
+        private Vector2 _last = Vector2.zero;
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            print(eventData.delta);
+        }
     }
 }
