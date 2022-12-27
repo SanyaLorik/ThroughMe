c502baf637359c76af44650623744cd91bfd3b1a
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2022-12-27 14:57:43 +0300

    fix: rotator

--- Assets/Project/Scripts/Movement/PortalRotator.cs
+++ Assets/Project/Scripts/Movement/PortalRotator.cs
@@ -29,7 +29,7 @@ namespace ThroughMe.Movement
 
         private void Rotate()
         {
-            float angle = Vector2.SignedAngle(Vector2.up, _direction.Direction) + _camera.eulerAngles.y;
+            float angle = -(Vector2.SignedAngle(Vector2.up, _direction.Direction) + _camera.eulerAngles.y);
             Quaternion quaternion = Quaternion.Euler(new Vector3(0, angle, 0));
             Portal.Body.MoveRotation(quaternion);
         }
